5ac0312ccb1dd65f224bc8f934ad4cf3fb9dc186
hooamo <hooamo@gmail.com>
2015-03-22 22:40:40 +0800

    0.1

--- ADO.Net/ADO.Net/SQLHelper.cs
+++ ADO.Net/ADO.Net/SQLHelper.cs
@@ -46,7 +46,9 @@ namespace ADO.Net
             }
         }
 
-        // SqlDataReader是连接相关的，相当于数据库的游标，智能读取当前游标指向的行，并非返回结果，当连接关闭的时候，ExecuteReader就不能读取了。优点是，对程序占用内存几乎没影响。因此，以下代码是错误的：应该使用DataSet
+        // SqlDataReader是连接相关的，相当于数据库的游标，智能读取当前游标指向的行，并非返回结果。
+        // 当连接关闭的时候，ExecuteReader就不能读取了。优点是，对程序占用内存几乎没影响。
+        // 因此，以下代码是错误的：应该使用DataSet
         
         /*public static SqlDataReader ExecuteReader(string sql, params SqlParameter[] parameters)
         {
